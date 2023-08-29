67395ee5df95a56c5408c8bf5f869bb9491a6681
daniil0040 <106932023+daniil0040@users.noreply.github.com>
2023-08-29 21:49:50 +0000

    Deploying to gh-pages from @ daniil0040/goit-js-hw-09@ac62f99fc43942a9786528657945ae87717eaec0 🚀

---
+++ 01-color-switcher.453de87d.js
@@ -0,0 +1,2 @@
+const t={startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]"),bg:document.querySelector("body")};t.startBtn.addEventListener("click",(function(){intervalId=setInterval((()=>{t.bg.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),t.startBtn.disabled=!0})),t.stopBtn.addEventListener("click",(function(){clearInterval(intervalId),t.startBtn.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.453de87d.js.map
