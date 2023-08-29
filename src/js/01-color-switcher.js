ac62f99fc43942a9786528657945ae87717eaec0
daniil0040 <danyapolishchuk777@gmail.com>
2023-08-29 16:49:16 -0500

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const selectors = {
+    startBtn: document.querySelector("[data-start]"),
+    stopBtn: document.querySelector("[data-stop]"),
+    bg: document.querySelector("body")
+}
+ selectors.startBtn.addEventListener("click",handlerStart)
+selectors.stopBtn.addEventListener("click",handlerStop)
 
+function handlerStart() {
+    intervalId = setInterval(() => {
+        selectors.bg.style.backgroundColor = `${getRandomHexColor()}`
+    }, 1000);
+    selectors.startBtn.disabled = true
+}
+function handlerStop() {
+    clearInterval(intervalId)
+    selectors.startBtn.disabled = false
+}
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
